80097aebec95401247f126f76d408c1b45ec429b
Tonion <thanhnguyen240202@gmail.com>
2025-05-03 16:27:32 +0700

    fix: update demo user seeder to hash password before insertion

--- server/src/seeders/20250503083922-demo-user.js
+++ server/src/seeders/20250503083922-demo-user.js
@@ -1,8 +1,11 @@
 'use strict';
+import bcrypt from 'bcryptjs';
 export async function up(queryInterface, Sequelize) {
+  const hashedPassword = await bcrypt.hash('Admin1234@', 10);
+
   await queryInterface.bulkInsert('Users', [{
     email: 'admin@example.com',
-    password: 'hashedPassword',
+    password: hashedPassword,
     name: 'Demo Admin',
     avatar: 'https://example.com/avatar.jpg',
     lastPasswordChange: new Date(),
